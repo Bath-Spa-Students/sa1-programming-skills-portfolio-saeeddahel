bb2463e1faa8a3db4ed220ddf82c230838baec36
saeeddahel <Blackymail31@gmail.com>
2025-03-01 12:29:26 +0400

    Update Exercise01.py

--- Exercise/Exercise01.py
+++ Exercise/Exercise01.py
@@ -1,8 +1,8 @@
-#assigning value to variable named word1 
+#assigning value to variable named word1 as Coding
 word1 = "Coding"
-#assigning value to the variable named word2
+#assigning value to the variable named word2 as is
 word2 = "is"
-#Assigning value to the variable named word3
+#Assigning value to the variable named word3 as cool
 word3 = "cool"
 #displaying the three varibales as a string concatenation
 print(word1 + " " + word2 + " " + word3)

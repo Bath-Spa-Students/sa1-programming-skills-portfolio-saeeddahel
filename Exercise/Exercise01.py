54344e823847dc7eb44b220adf6211609c0d9b56
saeeddahel <Blackymail31@gmail.com>
2025-03-01 12:23:53 +0400

    Coding is cool

---
+++ Exercise/Exercise01.py
@@ -0,0 +1,8 @@
+#assigning value to variable named word1 
+word1 = "Coding"
+#assigning value to the variable named word2
+word2 = "is"
+#Assigning value to the variable named word3
+word3 = "cool"
+#displaying the three varibales as a string concatenation
+print(word1 + " " + word2 + " " + word3)

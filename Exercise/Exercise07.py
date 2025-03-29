ef63389956f2f7344cb52de32fea9bb328cbfb1a
saeeddahel <Blackymail31@gmail.com>
2025-03-29 14:47:22 +0400

    Create Exercise07.py
    Some Counting

---
+++ Exercise/Exercise07.py
@@ -0,0 +1,24 @@
+# The code uses a `for` loop with a step of 1 to print numbers from 0 to 50 (inclusive)
+print("Counting up from 0 to 50:")
+for s in range(0, 51, 1):
+    print(s)
+
+# this code uses a `for` loop with a step of -1 to print numbers from 50 down to 0 (inclusive)
+print("Counting down from 50 to 0:")
+for s in range(50, -1, -1):
+    print(s)
+
+# The code uses a `for` loop with a step of 1 to print numbers from 30 to 50 (inclusive)
+print("Counting up from 30 to 50:")
+for s in range(30, 51, 1):
+    print(s)
+
+# The code uses a `for` loop to print even numbers in steps of -2 from 50 to 10 (inclusive)
+print("Counting down from 50 to 10 in decrements of 2:")
+for s in range(50, 9, -2):
+    print(s)
+
+# The code uses a `for` loop to print numbers in increments of 5 from 100 to 200 (inclusive)
+print("Counting up from 100 to 200 in increments of 5:")
+for s in range(100, 201, 5):
+    print(s)

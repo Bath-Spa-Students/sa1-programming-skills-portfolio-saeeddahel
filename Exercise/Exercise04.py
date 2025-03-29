c950aae59b7872e02f81d30b9d5daf6b17f3815b
saeeddahel <Blackymail31@gmail.com>
2025-03-29 14:45:35 +0400

    Create Exercise04.py
    Primitive Quiz

---
+++ Exercise/Exercise04.py
@@ -0,0 +1,79 @@
+# This code stores the user's response in the variable capital after requesting the capital of France
+capital = input("What is the capital of France: ")
+# This code determines whether the user's capitalized input is "paris" (case-insensitive)
+if capital.lower() == "paris":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+#This code stores the user's response in the variable capital after requesting the capital of China
+capital = input("What is the capital of China: ")  
+# This code determines whether the user's capitalized input is "Beijing" (case-insensitive)
+if capital.lower() == "beijing":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of Japan
+capital = input("What is the capital of Japan: ")  
+#This code determines whether the user's capitalized input is "tokyo" (case-insensitive)
+if capital.lower() == "tokyo":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of Germany
+capital = input("What is the capital of Germany: ")  
+#This code determines whether the user's capitalized input is "Berlin" (case-insensitive)
+if capital.lower() == "berlin":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of Spain
+capital = input("What is the capital of Spain: ")  
+#This code determines whether the user's capitalized input is "madrid" (case-insensitive)
+if capital.lower() == "madrid":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of Canada
+capital = input("What is the capital of Canada: ")  
+#This code determines whether the user's capitalized input is "ottawa" (case-insensitive)
+if capital.lower() == "ottawa":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of India
+capital = input("What is the capital of India: ")  
+#This code determines whether the user's capitalized input is "new delhi" (case-insensitive)
+if capital.lower() == "new delhi":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of Italy
+capital = input("What is the capital of Italy: ")  
+#This code determines whether the user's capitalized input is "rome" (case-insensitive)
+if capital.lower() == "rome":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of England
+capital = input("What is the capital of England: ")  
+#This code determines whether the user's capitalized input is "london" (case-insensitive)
+if capital.lower() == "london":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  
+
+# This code stores the user's response in the variable capital after requesting the capital of United Arab Emirates
+capital = input("What is the capital of the United Arab Emirates: ")  
+#This code determines whether the user's capitalized input is "abu dhabi" (case-insensitive)
+if capital.lower() == "abu dhabi":  
+    print("The answer is correct")  
+else:  
+    print("The answer is wrong")  

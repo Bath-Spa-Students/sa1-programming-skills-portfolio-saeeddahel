201b654b63b64b67ff726efb36ab0bfce9a30b41
saeeddahel <Blackymail31@gmail.com>
2025-03-29 14:45:06 +0400

    Create Exercise02.py
    Simple Sums

---
+++ Exercise/Exercise02.py
@@ -0,0 +1,8 @@
+# The code gives the variables `math_1` and `math_2` the values `8` and `10`, respectively
+math_1 = 8
+math_2 = 10
+
+# The code stores the result (18) in the variable `add` after adding the values of `math_1` (8) and `math_2` (10)
+add = math_1 + math_2
+#The value of `add` is inserted into the placeholder `{add}` in this code to print the formatted string `"Sum of the value: 18"`
+print(f"Sum of the value: {add}")
